f1916aa0d7ca8dfdabc9d085855f8837903c334e
SB <sbryukov@gmail.com>
2018-04-18 10:08:48 +0300

    initial

---
+++ src/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+     name='ctbf',    # This is the name of your PyPI-package.
+     version='0.1.4',
+     description='Crypto Trading Bot Platform',
+     author='Sergey Bryukov',
+     author_email='sbryukov@gmail.com',
+     license='MIT',
+     packages=['ctbf'],
+     zip_safe=False
+     )
